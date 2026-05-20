07350267a6155139735ba89d7d6fa4f8a908281a
formalParrot <a.e.makarov13@gmail.com>
2026-05-20 14:41:43 +0400

    ready

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,12 @@
-const { Events, ActivityType } = require('discord.js');
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
- name: Events.ClientReady,
- once: true,
- execute(client) {
- console.log(` Logged in as ${client.user.tag}`);
- client.user.setActivity('competitive events', { type: ActivityType.Watching });
- },
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(` Logged in as ${client.user.tag}`);
+    client.user.setActivity("competitive events", {
+      type: ActivityType.Custom,
+    });
+  },
 };
